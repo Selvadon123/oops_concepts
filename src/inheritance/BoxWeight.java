280e7c7e7e1a48b338ce8c40055daceffa37149e
Selvakumar S <selvasuki1234@gmail.com>
2023-04-28 20:28:22 +0530

    inheritance basic examaple with super keyword

---
+++ src/inheritance/BoxWeight.java
@@ -0,0 +1,15 @@
+package inheritance;
+
+public class BoxWeight extends Box{
+    double weight;
+
+    public BoxWeight() {
+        this.weight = -1;
+    }
+
+    public BoxWeight(double l, double h, double w, double weight) {
+        super(l, h, w);   //call the parent class constructor
+        //used to initialise values present in the parent class
+        this.weight = weight;
+    }
+}
--- src/inheritance/Main.java
+++ src/inheritance/Main.java
@@ -2,7 +2,10 @@ package inheritance;
 
 public class Main {
     public static void main(String[] args) {
-        Box box = new Box(4);
-        System.out.println(box.l+" "+ box.h+" "+ box.w);
+//        Box box = new Box(4);
+//        System.out.println(box.l+" "+ box.h+" "+ box.w);
+        BoxWeight b1=new BoxWeight();
+        BoxWeight b2=new BoxWeight(2,3,4,5);
+        System.out.println(b2.l+" "+ b2.h+" "+ b2.w+" "+b2.weight);
     }
 }
