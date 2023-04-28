74ca90bbaae23c48639118b833d042ccdd1de839
Selvakumar S <selvasuki1234@gmail.com>
2023-04-28 20:16:33 +0530

    oops basic exmaple

---
+++ src/inheritance/Main.java
@@ -0,0 +1,8 @@
+package inheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Box box = new Box(4);
+        System.out.println(box.l+" "+ box.h+" "+ box.w);
+    }
+}
